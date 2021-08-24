e290b7846274e74cc5a5b880da2abf56e5a217a2
topanrk <47352748+topanrk@users.noreply.github.com>
2021-08-24 18:12:25 +0700

    Update post-test.js

--- post-test.js
+++ post-test.js
@@ -1,12 +1,10 @@
 const p1 = document.getElementById('buttonSatu')
 
 p1.addEventListener ('click', function(){
-    const div = document.getElementById('a')
-    const div2 = document.getElementById('b')
-    const div3 = document.getElementById('c')
-    div.style.backgroundColor = 'blue'
-    div2.style.backgroundColor = 'lime'
-    div3.style.backgroundColor = 'green'
+    const paragraf = document.getElementsByTagName('p')
+    for (let i=0; i < paragraf.length; i++){
+        paragraf[i].style.background = 'orange'
+    }
 })
 
 const p2 =document.getElementById('buttonDua')
@@ -19,16 +17,19 @@ p2.addEventListener ('click', function(){
 const p3 = document.getElementById('buttonTiga')
 
 p3.addEventListener ('click', function(){
-    const btn = document.getElementsByTagName('body')[0]
-    const btn1 = document.getElementsByTagName('body')[0]
-    btn.style.fontFamily = 'times new roman'
-    btn1.style.color = 'red'
+    const paragraf3 = document.getElementsByTagName('p')
+    for (let i=0; i < paragraf3.length; i++){
+        paragraf3[i].style.fontFamily = 'Times New Roman'
+        paragraf3[i].style.color = 'lime'
+    }
 })
 
 const p4 = document.getElementById('buttonEmpat')
 p4.addEventListener ('click', function(){
-    const button = document.getElementsByTagName('body')[0]
-    button.style.fontSize = '25px'
+    const button = document.getElementsByTagName('p')
+    for (let i=0; i<button.length; i++){
+        button[i].style.fontSize = '25px'
+    }
 })
 
 const p5 = document.getElementById('buttonLima')
@@ -40,16 +41,28 @@ p5.addEventListener ('click', function(){
 const p6 = document.getElementById('buttonEnam')
 p6.addEventListener ('click', function(){
     const h2       = document.createElement('h2')
+    const tulisan  = document.createTextNode ('Selamat datang di Dojobox.id')
     const sectionC = document.getElementById('a')
+    const p1       = sectionC.getElementsByClassName('p1')[0]
+    
+    h2.appendChild(tulisan)    
 
-    h2.appendChild(sectionC)
-    const p        = sectionC.querySelector('p')
-
-    sectionC.replaceChild(h2, p)
+    sectionC.replaceChild(h2, p1)
 })
 
 const p7 = document.getElementById('buttonTujuh')
 p7.addEventListener ('click', function(){
+    const  elementYangAkanDiHapus = document.querySelector('#b ul')
+    const  eyad = document.querySelector('li:nth-child(1)')
+    elementYangAkanDiHapus.removeChild(eyad)
+
+    const tambah = document.createElement('li')
+    const isiTulisan = document.createTextNode('Topan')
+    tambah.appendChild(isiTulisan)
+
+    const li = document.querySelector('#b ul')
+    li.appendChild(tambah)
+    
 
 })
 
@@ -62,16 +75,27 @@ p8.addEventListener ('click', function(){
 const p9 = document.getElementById('buttonSembilan')
 p9.addEventListener ('click', function(){
     const sectionA = document.getElementById('a')
-    const elementYangAkanDiHapus = document.getElementsByTagName('a')[0]
-    sectionA.removeChild(elementYangAkanDiHapus)
+    const hapusLink = sectionA.querySelector('a')
+    hapusLink.removeAttribute('href')
 })
 
 const p10 = document.getElementById('buttonSembilan')
 p10.addEventListener ('click', function(){
-    const elementBaru = document.createElement('p')
-    const link = document.createTextNode('https://github.com/topanrk?tab=repositories')
+    // const newLi = document.createElement('a')
+    // const elementGit = document.createTextNode('Github Saya')
+    // const cy = document.querySelector('#c ul') 
 
-    elementBaru.appendChild(link)
-    const sectionAa = document.getElementById('a')
-    sectionAa.appendChild(elementBaru)
+    // newLi.appendChild(elementGit)    
+    // cy.appendChild(newLi)
+    // newLi.setAttribute('href','https://github.com/topanrk?tab=repositories')
+    
+    
+    const tambah1 = document.createElement('a')
+    const isiTulisan1 = document.createTextNode('Github Saya')
+    tambah1.appendChild(isiTulisan1)
+
+    const li1 = document.getElementsByTagName('a')
+    li1.appendChild(tambah1)
+    tambah1.setAttribute('href','https://github.com/topanrk?tab=repositories')
 })
+
